871a73e20828e1b51767688777b646573ce7d4d4
HITESH\hitesh <24716760+hiteshchouhan22@users.noreply.github.com>
2024-08-04 00:49:51 +0530

    init

---
+++ src/app/failure/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useEffect,useState } from "react";
+const Page = () => {
+  const router = useRouter();
+  const [timeLeft, setTimeLeft] = useState(4); // Initial timer set to 4 seconds
+
+  useEffect(() => {
+    // Set an interval to decrement the timer every second
+    const interval = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+
+    // Set a timeout to redirect after 4 seconds
+    const timer = setTimeout(() => {
+      router.push("http://localhost:3000");
+    }, 4000);
+
+    // Cleanup the interval and timeout on component unmount
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timer);
+    };
+  }, [router]);
+
+  return (
+    <div className="flex flex-col justify-center items-center text-center h-screen">
+      <br/>
+      <div>You payment has been declined use other methods to pay.</div>
+      <br/>
+      <div>Redirecting in {timeLeft} seconds...</div>
+    </div>
+  );
+};
+
+export default Page;
